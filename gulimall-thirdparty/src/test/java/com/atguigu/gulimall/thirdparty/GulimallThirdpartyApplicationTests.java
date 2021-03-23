3661167bce3a9ab2814c06cf0501314d96c6c2a1
GoGoGo <lwqMRL@163.com>
2021-03-23 11:06:44 +0800

    Update GulimallThirdpartyApplicationTests.java
    删除AccessId

--- gulimall-thirdparty/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdpartyApplicationTests.java
+++ gulimall-thirdparty/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdpartyApplicationTests.java
@@ -33,8 +33,8 @@
 //        // Endpoint以杭州为例，其它Region请按实际情况填写。
 ////        String endpoint = "oss-cn-shanghai.aliyuncs.com";
 ////// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-////        String accessKeyId = "LTAI4GJuHzLQoaCRymgp4Aj7";
-////        String accessKeySecret = "jmJAvgOONqONuSjeZg4PaBsDtOjO3R";
+////        String accessKeyId = "";
+////        String accessKeySecret = "";
 ////// 创建OSSClient实例。
 ////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 //// 上传文件流。
